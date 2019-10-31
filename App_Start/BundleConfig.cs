06b7296773110a77c3da1b6c9817f893bc73ea72
Mandana <mandanakhademi88@gmail.com>
2019-10-31 18:58:04 +0000

    improve the look and feel

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace GigHub
 {
@@ -24,7 +23,7 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
