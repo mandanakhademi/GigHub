9b4379751e92fdd824024ba52bb864f66a6099de
Mandana <mandanakhademi88@gmail.com>
2020-01-21 17:31:11 +0000

    display notifications by popover

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,7 +8,9 @@ namespace GigHub
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
