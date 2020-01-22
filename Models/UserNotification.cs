65174dfb731fe2ce04fcb92b058102f00cb9412d
Mandana <mandanakhademi88@gmail.com>
2020-01-22 16:14:01 +0000

    handle popover event to hide number of new notifications

--- Models/UserNotification.cs
+++ Models/UserNotification.cs
@@ -16,7 +16,7 @@ namespace GigHub.Models
 
         public ApplicationUser User { get; private set; }
         public Notification Notification { get; private set; }
-        public bool IsRead { get; set; }
+        public bool IsRead { get; private set; }
 
         protected UserNotification()
         {
@@ -32,8 +32,11 @@ namespace GigHub.Models
 
             User = user;
             Notification = notification;
+        }
 
-
+        public void Read()
+        {
+            IsRead = true;
         }
     }
 }
