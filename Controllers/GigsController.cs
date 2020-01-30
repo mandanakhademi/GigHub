a08b60fe885bf7a0bb6c54e085bca0878a79a8a9
Mandana <mandanakhademi88@gmail.com>
2020-01-30 16:48:00 +0000

    display details of a gig

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -17,6 +17,32 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == id);
+
+            if (gig == null)
+                return HttpNotFound();
+
+            var viewModel = new GigDetailsViewModel { Gig = gig };
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                viewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+
+                viewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+
+            return View("Details", viewModel);
+        }
+
         [Authorize]
         public ActionResult Mine()
         {
