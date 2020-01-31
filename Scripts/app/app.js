2f09a233d399107a1bb02f4faedb192dd232aacb
Mandana <mandanakhademi88@gmail.com>
2020-01-31 14:36:36 +0000

    clean the init method in app.js

--- Scripts/app/app.js
+++ Scripts/app/app.js
@@ -1,37 +1,39 @@
 ﻿var GigsController = function () {
 
     var init = function () {
-        $(".js-toggle-attendance").click(function (e) {
-            var button = $(e.target);
-            if (button.hasClass("btn-default")) {
-                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                    .done(function () {
-                        button
-                            .removeClass("btn-default")
-                            .addClass("btn-info")
-                            .text("Going")
-                    })
-                    .fail(function () {
-                        alert("Something failed!");
-                    });
-            } else {
-                $.ajax({
-                    url: "/api/attendances/" + button.attr("data-gig-id"),
-                    method: "DELETE"
-                })
-                    .done(function () {
-                        button
-                            .removeClass("btn-info")
-                            .addClass("btn-default")
-                            .text("Going?")
-                    })
-                    .fail(function () {
-                        alert("Something failed!");
-                    });
-            }
-
-        });
+        $(".js-toggle-attendance").click(toggleAttendance);
     };
+
+    var toggleAttendance = function (e) {
+        var button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                .done(function () {
+                    button
+                        .removeClass("btn-default")
+                        .addClass("btn-info")
+                        .text("Going")
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + button.attr("data-gig-id"),
+                method: "DELETE"
+            })
+                .done(function () {
+                    button
+                        .removeClass("btn-info")
+                        .addClass("btn-default")
+                        .text("Going?")
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        }
+
+    }
 
     return {
         init: init
