78e6ebc6e78a349425206e37b6edfb70d3743cbe
Mandana <mandanakhademi88@gmail.com>
2020-01-31 14:28:27 +0000

    refactor gigs view using revealing module pattern

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,6 +7,10 @@ namespace GigHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app/app.js"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
---
+++ Scripts/app/app.js
@@ -0,0 +1,40 @@
+﻿var GigsController = function () {
+
+    var init = function () {
+        $(".js-toggle-attendance").click(function (e) {
+            var button = $(e.target);
+            if (button.hasClass("btn-default")) {
+                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                    .done(function () {
+                        button
+                            .removeClass("btn-default")
+                            .addClass("btn-info")
+                            .text("Going")
+                    })
+                    .fail(function () {
+                        alert("Something failed!");
+                    });
+            } else {
+                $.ajax({
+                    url: "/api/attendances/" + button.attr("data-gig-id"),
+                    method: "DELETE"
+                })
+                    .done(function () {
+                        button
+                            .removeClass("btn-info")
+                            .addClass("btn-default")
+                            .text("Going?")
+                    })
+                    .fail(function () {
+                        alert("Something failed!");
+                    });
+            }
+
+        });
+    };
+
+    return {
+        init: init
+    }
+}();
+
