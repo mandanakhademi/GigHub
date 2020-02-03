5af01adfd8186e74be641a2d7218d4776e25c86f
Mandana <mandanakhademi88@gmail.com>
2020-02-03 16:39:32 +0000

    apply dry principle to toggleAttendance function

--- Scripts/app/app.js
+++ Scripts/app/app.js
@@ -1,38 +1,36 @@
 ﻿var GigsController = function () {
+    var button;
 
     var init = function () {
         $(".js-toggle-attendance").click(toggleAttendance);
     };
+        
 
     var toggleAttendance = function (e) {
-        var button = $(e.target);
+        button = $(e.target);
         if (button.hasClass("btn-default")) {
             $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                .done(function () {
-                    button
-                        .removeClass("btn-default")
-                        .addClass("btn-info")
-                        .text("Going")
-                })
-                .fail(function () {
-                    alert("Something failed!");
-                });
+                .done(done)
+                .fail(fail);
         } else {
             $.ajax({
                 url: "/api/attendances/" + button.attr("data-gig-id"),
                 method: "DELETE"
             })
-                .done(function () {
-                    button
-                        .removeClass("btn-info")
-                        .addClass("btn-default")
-                        .text("Going?")
-                })
-                .fail(function () {
-                    alert("Something failed!");
-                });
+                .done(done)
+                .fail(fail);
         }
 
+    }
+
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    }
+
+    var fail = function () {
+        alert("Something failed!");
     }
 
     return {
