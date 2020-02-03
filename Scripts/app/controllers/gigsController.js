d3fad7da90d3481741c19f5c86aa753163fd72e9
Mandana <mandanakhademi88@gmail.com>
2020-02-03 17:24:14 +0000

    move attendanceService and gigsController into separate files

---
+++ Scripts/app/controllers/gigsController.js
@@ -0,0 +1,35 @@
+﻿var GigsController = function (attendanceService) {
+    var button;
+
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
+
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+
+        var gigId = button.attr("data-gig-id");
+
+        if (button.hasClass("btn-default"))
+            attendanceService.createAttendance(gigId, done, fail);
+        else
+            attendanceService.deleteAttendance(gigId, done, fail);
+
+    }
+
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    }
+
+    var fail = function () {
+        alert("Something failed!");
+    }
+
+    return {
+        init: init
+    }
+}(AttendanceService);
+
