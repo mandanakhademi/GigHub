ae3b4b8e8367a9300c8754ce3e1aa4063bb13cd9
Mandana <mandanakhademi88@gmail.com>
2020-02-13 18:58:59 +0000

    mock the current user

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+        public GigsControllerTests()
+        {
+            var mockUoW = new Mock<IUnitOfWork>();
+            _controller = new GigsController(mockUoW.Object);
+            _controller.MockCurrentUser("1", "user1@domain.com");
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
