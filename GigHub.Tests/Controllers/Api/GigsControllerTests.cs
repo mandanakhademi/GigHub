ef4bc7c574e4e73f228f5959e1d7c9800045c6b1
Mandana <mandanakhademi88@gmail.com>
2020-02-17 18:19:57 +0000

    add fourth unit test of cancel action in gigs controller

--- GigHub.Tests/Controllers/Api/GigsControllerTests.cs
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -62,5 +62,17 @@ namespace GigHub.Tests.Controllers.Api
             result.Should().BeOfType<UnauthorizedResult>();
         }
 
+        [TestMethod]
+        public void Cancel_ValidRequest_ShouldReturnOk()
+        {
+            var gig = new Gig { ArtistId = _userId };
+
+            _mockRepository.Setup(r => r.GetGigWithAttendees(1)).Returns(gig);
+
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<OkResult>();
+        }
+
     }
 }
