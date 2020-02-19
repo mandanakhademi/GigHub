04136993b02a7de5a2f6cbd2c00c598efbffcdbf
Mandana <mandanakhademi88@gmail.com>
2020-02-19 17:22:33 +0000

    Add a unit test for gig repository

--- GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
+++ GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -1,8 +1,11 @@
-﻿using GigHub.Core.Models;
+﻿using FluentAssertions;
+using GigHub.Core.Models;
 using GigHub.Persistence;
 using GigHub.Persistence.Repositories;
+using GigHub.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Data.Entity;
 
 namespace GigHub.Tests.Persistence.Repositories
@@ -11,15 +14,28 @@ namespace GigHub.Tests.Persistence.Repositories
     public class GigRepositoryTests
     {
         private GigRepository _repository;
+        private Mock<DbSet<Gig>> _mockGigs;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            var mockGigs = new Mock<DbSet<Gig>>();
+            _mockGigs = new Mock<DbSet<Gig>>();
             var mockContext = new Mock<IApplicationDbContext>();
-            mockContext.SetupGet(c => c.Gigs).Returns(mockGigs.Object);
+            mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
 
             _repository = new GigRepository(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(-1), ArtistId = "1" };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUpcomingGigsByArtist("1");
+
+            gigs.Should().BeEmpty();
         }
     }
 }
