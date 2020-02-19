cb1b18926e3edd7ebd8f451bcbe25a3e9182b3c8
Mandana <mandanakhademi88@gmail.com>
2020-02-19 17:04:40 +0000

    Mock db context and db set for testing repositories

---
+++ GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+
+namespace GigHub.Tests.Persistence.Repositories
+{
+    [TestClass]
+    public class GigRepositoryTests
+    {
+        private GigRepository _repository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var mockGigs = new Mock<DbSet<Gig>>();
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.Gigs).Returns(mockGigs.Object);
+
+            _repository = new GigRepository(mockContext.Object);
+        }
+    }
+}
