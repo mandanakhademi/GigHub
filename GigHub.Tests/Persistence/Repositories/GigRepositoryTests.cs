72a4bef0d0231d15309496c9f9f67e128e9fe005
Mandana <mandanakhademi88@gmail.com>
2020-02-19 18:34:54 +0000

    Add a unit test to gig repository

--- GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
+++ GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -51,5 +51,17 @@ namespace GigHub.Tests.Persistence.Repositories
             gigs.Should().BeEmpty();
 
         }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsForADifferentArtist_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1), ArtistId = "1" };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUpcomingGigsByArtist(gig.ArtistId + "-");
+
+            gigs.Should().BeEmpty();
+        }
     }
 }
