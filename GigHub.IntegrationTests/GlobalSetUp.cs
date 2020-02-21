445c550a4237f891f48edf190d41a926a4ac2ef0
Mandana <mandanakhademi88@gmail.com>
2020-02-21 18:26:54 +0000

    Set up integration test

---
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace GigHub.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetUp
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var configuration = new GigHub.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+    }
+}
---
+++ GigHub.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c570853e-ea62-4351-9581-1bef9dedd090")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
