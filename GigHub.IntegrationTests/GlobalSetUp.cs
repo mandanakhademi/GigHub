83f827c25b433c2235197b3bb13ac2e738bb2ba1
Mandana <mandanakhademi88@gmail.com>
2020-02-21 18:56:38 +0000

    Add seeding data function

--- GigHub.IntegrationTests/GlobalSetUp.cs
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+using NUnit.Framework;
 using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace GigHub.IntegrationTests
 {
@@ -9,9 +12,28 @@ namespace GigHub.IntegrationTests
         [OneTimeSetUp]
         public void Init()
         {
+            MigrateDbToLatestVersion();
+            Seed();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
             var configuration = new GigHub.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+            context.SaveChanges();
+
         }
     }
 }
