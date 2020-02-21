876f063d30cb8ee120c71bd2ff7ba60a0c74ef41
Mandana <mandanakhademi88@gmail.com>
2020-02-21 19:05:17 +0000

    Add isolating test data function

---
+++ GigHub.IntegrationTests/Isolated.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System.Transactions;
+
+namespace GigHub.IntegrationTests
+{
+    public class Isolated : TestActionAttribute
+    {
+        private TransactionScope _transactionScope;
+
+        public override void BeforeTest(ITest test)
+        {
+            base.BeforeTest(test);
+        }
+
+        public override void AfterTest(ITest test)
+        {
+            base.AfterTest(test);
+        }
+
+        
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+}
