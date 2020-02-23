34343d83751f5401f9ad4d0f46359e8c9e7c4c49
Mandana <mandanakhademi88@gmail.com>
2020-02-23 18:42:26 +0000

    Add integration test for mine action in gigs controller

---
+++ GigHub.IntegrationTests/Controllers/GigsControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using GigHub.Controllers;
+using GigHub.Core.Models;
+using GigHub.IntegrationTests.Extensions;
+using GigHub.Persistence;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GigHub.IntegrationTests.Controllers
+{
+    [TestFixture]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+        private ApplicationDbContext _context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _context = new ApplicationDbContext();
+            _controller = new GigsController(new UnitOfWork(_context));
+        }
+        
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+
+        [Test, Isolated]
+        public void Mine_WhenCalled_ShouldReturnUpcomingGigs()
+        {
+            //Arrange
+            var user = _context.Users.First();
+            _controller.MockCurrentUser(user.Id, user.UserName);
+
+            var genre = _context.Genres.First();
+            var gig = new Gig { Artist = user, DateTime = DateTime.Now.AddDays(1), Genre = genre, Venue = "_" };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            //Act
+            var result = _controller.Mine();
+
+            //Assert
+            (result.ViewData.Model as IEnumerable<Gig>).Should().HaveCount(1);
+        }
+    }
+}
